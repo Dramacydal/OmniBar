762f39629f768b6615765d9e1147210c8ab3a5b4
Jordon <jordon@localhost>
2015-06-19 16:15:29 -0700

    add Blinding Light

--- OmniBar.lua
+++ OmniBar.lua
@@ -36,6 +36,7 @@ local cooldowns = {
 	[96231]  = { default = true,  duration = 15,  class = "PALADIN" },                                       -- Rebuke
 	[114039] = { default = false, duration = 30,  class = "PALADIN" },                                       -- Hand of Purity
 	[114157] = { default = false, duration = 60,  class = "PALADIN" },                                       -- Execution Sentence
+	[115750] = { default = false, duration = 120, class = "PALADIN" },                                       -- Blinding Light
 	[871]    = { default = false, duration = 180, class = "WARRIOR", specID = { 73 } },                      -- Shield Wall
 	[1719]   = { default = false, duration = 180, class = "WARRIOR", specID = { 71, 72 } },                  -- Recklessness
 	[3411]   = { default = false, duration = 30,  class = "WARRIOR" },                                       -- Intervene
